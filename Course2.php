19526bc92d169a5401f55a2b2b8b89fcd4946a30
Ike85 <B00165817@mytudublin.ie>
2024-02-05 12:29:14 +0000

    Create Course2.php

---
+++ Course2.php
@@ -0,0 +1,6 @@
+<u2 class="nav navbar-nav">
+    <li class="active"><a href="/">Home</a></li>
+    <li><a href="/pets_new.php">Post</a></li>
+    <li><a href="contact">Contact</a></li>
+    <li><a href="about">About</a></li>
+</u2>
